cce1c3168fb1d24ed690ab782f102ae61dc8591d
root <root@MANGIN-PC.INTERNAL.COMLINKDATA.COM>
2019-08-29 13:39:13 -0400

    day of draft changes

--- draft/draftday/input/.R
+++ draft/draftday/input/.R
@@ -1,118 +1,118 @@
-
-## --
-## draft day data input app
-##
-## --
-
-# global ----
-
-library(rhandsontable)
-library(shiny)
-library(data.table)
-
-source("../../.R")
-
-# data load ----
-
-load(file = "../../.RData")
-
-# data entry dashboard ---
-
-editTable <- function(DF, outdir=getwd(), outfilename="table"){
-
-  ui <- shinyUI(fluidPage(
-
-    titlePanel("Draft Pick Entries"),
-    sidebarLayout(
-      sidebarPanel(
-        helpText("Enter and Save"),
-        br(), 
-
-        wellPanel(
-          h3("Save"), 
-          actionButton("save", "Save table")
-        )        
-
-      ),
-
-      mainPanel(
-
-        rHandsontableOutput("hot")
-
+  
+  ## --
+  ## draft day data input app
+  ##
+  ## --
+  
+  # global ----
+  
+  library(rhandsontable)
+  library(shiny)
+  library(data.table)
+  
+  source("../../.R")
+  
+  # data load ----
+  
+  load(file = "../../.RData")
+  
+  # data entry dashboard ---
+  
+  editTable <- function(DF, outdir=getwd(), outfilename="table"){
+  
+    ui <- shinyUI(fluidPage(
+  
+      titlePanel("Draft Pick Entries"),
+      sidebarLayout(
+        sidebarPanel(
+          helpText("Enter and Save"),
+          br(), 
+  
+          wellPanel(
+            h3("Save"), 
+            actionButton("save", "Save table")
+          )        
+  
+        ),
+  
+        mainPanel(
+  
+          rHandsontableOutput("hot")
+  
+        )
+      )
+    ))
+  
+    server <- shinyServer(function(input, output) {
+  
+      values <- reactiveValues()
+  
+      ## Handsontable
+      observe({
+        if (!is.null(input$hot)) {
+          DF = hot_to_r(input$hot)
+        } else {
+          if (is.null(values[["DF"]]))
+            DF <- DF
+          else
+            DF <- values[["DF"]]
+        }
+        values[["DF"]] <- DF
+      })
+  
+      output$hot <- renderRHandsontable({
+        DF <- values[["DF"]]
+        if (!is.null(DF))
+          rhandsontable(DF, useTypes = TRUE, stretchH = "all") %>%
+          hot_col(
+            col = "first_name"
+            ,type = "autocomplete"
+            ,source = unique(p$first_name)
+            ,strict = TRUE
+          ) %>%
+          hot_col(
+            col = "last_name"
+            ,type = "autocomplete"
+            ,source = unique(p$last_name)
+            ,strict = TRUE
+          ) %>%
+          hot_col(
+            col = "position"
+            ,type = "autocomplete"
+            ,source = unique(p$position)
+            ,strict = TRUE
+          )
+      })
+  
+      ## Save 
+      observeEvent(input$save, {
+        finalDF <- isolate(values[["DF"]])
+        saveRDS(
+          finalDF
+          ,file = file.path(outdir, sprintf("%s.rds", outfilename)))
+      })
+  
+    })
+  
+    ## run app 
+    runApp(list(ui=ui, server=server))
+    return(invisible())
+  }
+  
+  draft_input <- 
+    data.table(
+      cbind(
+        "pick" = seq(1,180)
+        ,"dp" = 0
+        ,"first_name" = ""
+        ,"last_name" = ""
+        ,"position" = ""
       )
     )
-  ))
-
-  server <- shinyServer(function(input, output) {
-
-    values <- reactiveValues()
-
-    ## Handsontable
-    observe({
-      if (!is.null(input$hot)) {
-        DF = hot_to_r(input$hot)
-      } else {
-        if (is.null(values[["DF"]]))
-          DF <- DF
-        else
-          DF <- values[["DF"]]
-      }
-      values[["DF"]] <- DF
-    })
-
-    output$hot <- renderRHandsontable({
-      DF <- values[["DF"]]
-      if (!is.null(DF))
-        rhandsontable(DF, useTypes = TRUE, stretchH = "all") %>%
-        hot_col(
-          col = "first_name"
-          ,type = "autocomplete"
-          ,source = unique(p$first_name)
-          ,strict = TRUE
-        ) %>%
-        hot_col(
-          col = "last_name"
-          ,type = "autocomplete"
-          ,source = unique(p$last_name)
-          ,strict = TRUE
-        ) %>%
-        hot_col(
-          col = "position"
-          ,type = "autocomplete"
-          ,source = unique(p$position)
-          ,strict = TRUE
-        )
-    })
-
-    ## Save 
-    observeEvent(input$save, {
-      finalDF <- isolate(values[["DF"]])
-      saveRDS(
-        finalDF
-        ,file = file.path(outdir, sprintf("%s.rds", outfilename)))
-    })
-
-  })
-
-  ## run app 
-  runApp(list(ui=ui, server=server))
-  return(invisible())
-}
-
-draft_input <- 
-  data.table(
-    cbind(
-      "pick" = seq(1,180)
-      ,"dp" = 0
-      ,"first_name" = ""
-      ,"last_name" = ""
-      ,"position" = ""
-    )
-  )
-
-for(i in 1:180){
-draft_input[pick == i,dp := which(snake ==i,arr.ind = TRUE)[1] ]
-}
   
-editTable(draft_input,outdir = "../picks/",outfilename = "current")
+  for(i in 1:180){
+  draft_input[pick == i,dp := which(snake ==i,arr.ind = TRUE)[1] ]
+  }
+    
+  editTable(draft_input,outdir = "../picks/",outfilename = "current")
 
--- draft/draftday/picks/.R
+++ draft/draftday/picks/.R
@@ -15,6 +15,12 @@ source("../../.R")
 load(file = "../../.RData")
 
 # set up draft data ----
+
+# no Free Agents
+p <- p[team != "FA",]
+p <- p[team != "FA*",]
+p <- p[last_name != "Luck",]
+
   
 draft <- 
   p[
@@ -51,7 +57,7 @@ draft[,dp:=0]
 
 # draft picks ----
 
-# pick #2
+# pick #2 ROUND 1
 
 pos_accum <- update_draft()
 
@@ -59,14 +65,86 @@ pos_accum <- c("RB" = 0,"QB" = 0,"WR" = 0,"TE" = 0,"DST" = 0)
 
 draft_pick(2)
 
-# pick #23
+# pick #23 ROUND 2
 
 pos_accum <- update_draft()
 
-draft_pick(23)
+draft_pick()
 
-# pick #26
+# pick #26 ROUND 3
 
 pos_accum <- update_draft()
 
 draft_pick(26)
+
+# pick #47 ROUND 4
+
+pos_accum <- update_draft()
+
+draft_pick(47)
+
+# pick #50 ROUND 5
+
+pos_accum <- update_draft()
+
+draft_pick(50)
+
+# pick #71 ROUND 6
+
+pos_accum <- update_draft()
+
+draft_pick(71)
+
+# pick #74 ROUND 7
+
+pos_accum <- update_draft()
+
+draft_pick(74)
+
+# pick #95 ROUND 8
+
+pos_accum <- update_draft()
+
+draft_pick(95)
+
+# pick #98 ROUND 9
+
+pos_accum <- update_draft()
+
+draft_pick(98)
+
+# pick #119 ROUND 10
+
+pos_accum <- update_draft()
+
+draft_pick(119)
+
+# pick #122 ROUND 11
+
+pos_accum <- update_draft()
+
+draft_pick(122)
+
+# pick #143 ROUND 12
+
+pos_accum <- update_draft()
+
+draft_pick(143)
+
+# pick #146 ROUND 13
+
+pos_accum <- update_draft()
+
+draft_pick(146)
+
+# pick #167 ROUND 14
+
+pos_accum <- update_draft()
+
+draft_pick(167)
+
+# pick #170 ROUND 15
+
+pos_accum <- update_draft()
+
+draft_pick(170)
