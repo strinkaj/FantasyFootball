c9f35b4b6fd3062e44e2fe1c8b87888061e5af64
root <root@MANGIN-PC.INTERNAL.COMLINKDATA.COM>
2019-09-04 09:48:57 -0400

    ran data input wioth league rules

--- input/.R
+++ input/.R
@@ -93,35 +93,35 @@ rules <-
         ,pass_inc = 0
         ,pass_yds = 0.04
         ,pass_tds = 4
-        ,pass_int = -1
+        ,pass_int = -2
         ,pass_40_yds = 0
         ,pass_300_yds = 0
-        ,pass_350_yds = 0
-        ,pass_400_yds = 0
+        ,pass_350_yds = 1
+        ,pass_400_yds = 1
       )
     
     ,rush =
       list(
         all_pos = TRUE
         ,rush_yds = 0.1
-        ,rush_att = 0
+        ,rush_att = 0.1
         ,rush_40_yds = 0
         ,rush_tds = 6
         ,rush_100_yds = 0
-        ,rush_150_yds = 0
-        ,rush_200_yds = 0
+        ,rush_150_yds = 1
+        ,rush_200_yds = 1
       )
       
     ,rec = 
       list(
         all_pos = TRUE
-        ,rec = 0.5
+        ,rec = 1
         ,rec_yds = 0.1
         ,rec_tds = 6
         ,rec_40_yds = 0
         ,rec_100_yds = 0
-        ,rec_150_yds = 0
-        ,rec_200_yds = 0
+        ,rec_150_yds = 1
+        ,rec_200_yds = 1
       )
       
     ,misc = 
@@ -139,9 +139,9 @@ rules <-
         ,fg_0019 = 3.0
         ,fg_2029 = 3.0
         ,fg_3039 = 3.0
-        ,fg_4049 = 4.0
+        ,fg_4049 = 4
         ,fg_50 = 5.0
-        ,fg_miss = 0.0
+        ,fg_miss = -1
       )
       
     ,ret =
@@ -178,12 +178,16 @@ rules <-
       
     ,pts_bracket =
       list(
-        list(threshold = 0, points = 7)
-        ,list(threshold = 6, points = 4)
-        ,list(threshold = 20, points = 1)
-        ,list(threshold = 27, points = 0)
+        list(threshold = 0, points = 8)
+        ,list(threshold = 6, points = 6)
+        ,list(threshold = 13, points = 4)
+        ,list(threshold = 14, points = 2)
+        ,list(threshold = 17, points = 2)
+        ,list(threshold = 21, points = 2)
+        ,list(threshold = 27, points = 1)
         ,list(threshold = 34, points = -1)
-        ,list(threshold = 99, points = -4)
+        ,list(threshold = 45, points = -2)
+        ,list(threshold = 99, points = -3)
       )
 
 )
