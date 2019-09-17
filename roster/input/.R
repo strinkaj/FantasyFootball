d690ac40b88cee9e375dcebb14e9708196ad0678
root <root@MANGIN-PC.INTERNAL.COMLINKDATA.COM>
2019-09-16 22:52:07 -0400

    ran full 16 weeks

--- roster/input/.R
+++ roster/input/.R
@@ -13,9 +13,30 @@ library(data.table)
 
 load(file = "../../input/.RData")
 
+# check current week
+
+current_week
+
+weeks_to_end
 
 d <- 
-  p3[
+  p_future[
+    avg_type == "weighted"
+    ,.(
+    week
+    ,id
+    ,first_name
+    ,last_name
+    ,team
+    ,position
+    ,floor
+    ,points
+    ,ceiling
+    )
+  ]
+
+d0 <- 
+  p_current[
     avg_type == "weighted"
     ,.(
     id
@@ -28,6 +49,46 @@ d <-
     ,ceiling
     )
   ]
+
+d[ ,score := 0.9^(week - current_week)*ceiling^(9/12)*points^(2/12)*floor^(1/12)]
+
+d0[ ,score := ceiling^(9/12)*points^(2/12)*floor^(1/12)]
+
+d <-
+  d[
+    ,.(
+      floor = sum(floor)
+      ,points = sum(points)
+      ,score = sum(score)
+      ,ceiling = sum(ceiling)
+      )
+    ,by =
+      .(
+      id
+      ,first_name
+      ,last_name
+      ,team
+      ,position
+      ) 
+    ]
+
+d0 <-
+  d0[
+    ,.(
+      floor = sum(floor)
+      ,points = sum(points)
+      ,score = sum(score)
+      ,ceiling = sum(ceiling)
+      )
+    ,by =
+      .(
+      id
+      ,first_name
+      ,last_name
+      ,team
+      ,position
+      ) 
+    ]
 
 # roster updates
 
@@ -145,62 +206,184 @@ d[
   )
 ]
 
-d[
-  (team != "FA")
-  &(drafted == 0|dp == 8)
-  &(position == "WR"|position == "TE")
-,][
-  order(-(ceiling^(5/9)*points^(3/9)*floor^(1/9)))
-,][
-  1:24
-  ,.(
-    dp
-    ,first_name
+d0[,drafted:=0]
+
+d0[,dp:=0]
+
+d0[
+  input
+  ,`:=`(
+    drafted = 1
+    ,dp = as.numeric(i.dp)
+  )
+  ,on = 
+    .(
+    first_name
     ,last_name
     ,position
-    ,team
-    ,"10%" = round(floor,2)
-    ,"avg" = round(ceiling^(5/9)*points^(3/9)*floor^(1/9),2)
-    ,"90%" = round(ceiling,2)
   )
 ]
 
-d[
-  (team != "FA")
-  &(drafted == 0|dp == 8)
-  &(position == "RB")
-,][
-  order(-(ceiling^(5/9)*points^(3/9)*floor^(1/9)))
-,][
-  1:24
-  ,.(
-    dp
-    ,first_name
-    ,last_name
-    ,position
-    ,team
-    ,"10%" = round(floor,0)
-    ,"avg" = round(ceiling^(5/9)*points^(3/9)*floor^(1/9),2)
-    ,"90%" = round(ceiling,0)
-  )
-]
+wr_avail <- 
+  d[drafted == 0 & position == "WR",
+  ][order(-score),]
 
-d[
-  (team != "FA")
-  &(drafted == 0|dp == 8)
-  &(position == "DST")
-,][
-  order(-(ceiling^(5/9)*points^(3/9)*floor^(1/9)))
-,][
-  1:10
-  ,.(
-    dp
-    ,first_name
-    ,last_name
-    ,position
-    ,team
-    ,"10%" = round(floor,0)
-    ,"avg" = round(ceiling^(5/9)*points^(3/9)*floor^(1/9),2)
-    ,"90%" = round(ceiling,0)
+n <- 
+  wr_avail[
+    (round(score,0) - 1  > 
+       (d[
+          dp == 8
+          & position == "WR"
+        ,][order(score)
+        ,][1,score]
+        ) 
+    ),.N ]
+
+for (i in 1:n){
+  
+print(
+  cat(
+    "\t \t \t \t \t "
+    ,unlist(
+      wr_avail[i,.(first_name,last_name,round(score,0))]
+    )
   )
+)
+
+print(
+  d[
+    dp == 8 & position == "WR" & score + 1 < 
+      wr_avail[
+        i,round(score,0)],
+      ][order(score),
+      ][1:i, 
+        .(
+          first_name
+          ,last_name
+          ,"score" = round(score,0)
+        )
+      ][!is.na(first_name),]
+)
+
+}
+
+rb_avail <- d[drafted==0&position=="RB",][order(-score),]
+
+n <- rb_avail[(round(score,0) - 1  > (d[dp==8&position=="RB",][order(score),][1,score]) ) ,.N ]
+
+for (i in 1:n){
+  
+print(cat("\t \t \t \t \t ",unlist(rb_avail[i,.(first_name,last_name,round(score,0))])))
+print(d[dp==8&position=="RB"&score + 1 < rb_avail[i,round(score,0)],][order(score),][1:i, .(first_name,last_name,"score" = round(score,0))][!is.na(first_name),])
+
+}
+
+te_avail <- d[drafted==0&position=="TE",][order(-score),]
+
+n <- te_avail[(score - 1  > (d[dp==8&position=="TE",][order(score),][1,"score" = round(score,0)]) ) ,.N ]
+
+for (i in 1:n){
+  
+print(cat("\t \t \t \t \t ",unlist(te_avail[i,.(first_name,last_name,round(score,0))])))
+print(d[dp==8&position=="TE"&score + 1 < te_avail[i,round(score,0)],][order(score),][1:i, .(first_name,last_name,round(score,0))][!is.na(first_name),])
+
+}
+
+
+
+View(
+d[
+(team != "FA")
+&(drafted == 0|dp == 8)
+&(position == "WR")
+,
+][
+order(-score),
+][
+1:35
+,.(
+  dp
+  ,first_name
+  ,last_name
+  ,team
+  ,position
+  ,"floor" = round(floor,0)
+  ,"points" = round(points,0)
+  ,"score" = round(score,0)
+  ,"ceiling" = round(ceiling,0)
+)
 ]
+)
+
+View(
+d[
+(team != "FA")
+&(drafted == 0|dp == 8)
+&(position == "RB")
+,
+][
+order(-score),
+][
+1:10
+,.(
+  dp
+  ,first_name
+  ,last_name
+  ,team
+  ,position
+  ,"floor" = round(floor,0)
+  ,"points" = round(points,0)
+  ,"score" = round(score,0)
+  ,"ceiling" = round(ceiling,0)
+)
+]
+)
+
+View(
+d[
+(team != "FA")
+&(drafted == 0|dp == 8)
+&(position == "TE")
+,
+][
+order(-score),
+][
+1:10
+,.(
+  dp
+  ,first_name
+  ,last_name
+  ,team
+  ,position
+  ,"floor" = round(floor,0)
+  ,"points" = round(points,0)
+  ,"score" = round(score,0)
+  ,"ceiling" = round(ceiling,0)
+)
+]
+)
+
+View(
+d0[
+(team != "FA")
+&(drafted == 0|dp == 8)
+&(position == "DST")
+,
+][
+order(-score),
+][
+1:20
+,.(
+  dp
+  ,first_name
+  ,last_name
+  ,team
+  ,position
+  ,"floor" = round(floor,0)
+  ,"points" = round(points,0)
+  ,"score" = round(score,0)
+  ,"ceiling" = round(ceiling,0)
+)
+]
+)
+
